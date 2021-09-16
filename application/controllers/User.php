d2671ddaa5d9afc99438a5a93e82a3d63228feb3
Elin Pablico <dokyumento.developers@gmail.com>
2021-09-16 18:27:21 +0800

    make it better

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,10 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class User extends CI_Controller{
     
-    function __construct(){
-        parent::__construct();
-         $this->load->model('user_model','user');
-    }
+  //  function __construct(){
+    //    parent::__construct();
+     //    $this->load->model('user_model','user');
+   // }
     public function index(){
         $this->load->view('template/header');
         $this->load->view('register/index');
