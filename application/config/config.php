aa90d82f190691287ec8da73f1e686be5eddec75
Elin Pablico <dokyumento.developers@gmail.com>
2021-09-16 18:41:05 +0800

    make it better

--- application/config/config.php
+++ application/config/config.php
@@ -377,14 +377,14 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database'; //files
+$config['sess_driver'] = 'files'; //files
 $config['sess_cookie_name'] = 'ci_sessions';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = APPPATH.'_ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-$config['sess_use_database'] = TRUE;
+//$config['sess_use_database'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
