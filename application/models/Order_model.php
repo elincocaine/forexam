f6132c8626bf8f9a5e1d628d33a63c1e138a4b1e
Elin Pablico <dokyumento.developers@gmail.com>
2021-09-21 12:59:11 +0800

    fix query

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -7,7 +7,8 @@ class Order_model extends CI_Model{
 
 	public function fetchAllPending(){
             $this->db->where('status',2);
-	$query = $this->db->get('order_request');
+            $this->db->join('inventory i', 'i.id = or.itemid');
+	$query = $this->db->get('order_request or');
 
         if($query->num_rows() > 0){
             return $query->result();
